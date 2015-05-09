894eb814a219f27ee81717c0995c154cc7f8dc41
shoya <12c1055@gmail.com>
2015-05-09 18:39:46 +0900

    add function

---
+++ RealmFire/Realm-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  Realm-Bridging-Header.h
+//  RealmFire
+//
+//  Created by shoya on 5/9/15.
+//  Copyright (c) 2015 com.moru. All rights reserved.
+//
+
+#ifndef RealmFire_Realm_Bridging_Header_h
+#define RealmFire_Realm_Bridging_Header_h
+
+
+#endif
+
+
+#import <Firebase/Firebase.h>
--- RealmFire/ViewController.swift
+++ RealmFire/ViewController.swift
@@ -9,25 +9,80 @@
 import UIKit
 import RealmSwift
 
-
-class Book: Object {
-    dynamic var title = ""
-    dynamic var author = ""
+class Note: Object {
+    dynamic var id = "3"
+    dynamic var body = ""
+    
+    override static func primaryKey() -> String? {
+        return "id"
+    }
 }
 
-
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    private var textField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupTextField("init")
     }
+    
+    
+    func textFieldDidBeginEditing(textField: UITextField){
+        println("textFieldDidBeginEditing: \(textField.text)")
+    }
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+    
+        let firebaseUrl = "https://realmfire.firebaseio.com/note"
+        var firebaseRef = Firebase(url:firebaseUrl)
+    
+        var realm = Realm(path: "/Users/shoya/Documents/RealmFire/db.realm")
+        
+        let note = Note()
+        note.id = "1"
+        note.body = textField.text
+        
+        
+        // 'The Realm is already in a write transaction' が発生する
+        /*
+        realm.beginWrite()
+        realm.add(note, update: true)
+        
+        firebaseRef.setValue(textField.text, withCompletionBlock: {
+            (error:NSError?, ref:Firebase!) in
+            if (error != nil) {
+                realm.cancelWrite()
+                println("could not be saved.")
+            } else {
+                realm.commitWrite()
+                println("saved successfully!")
+            }
+        })
+        */
+        
+        // とりあえずこれ
+        realm.write { () -> Void in
+            realm.add(note, update: true)
+        }
+        
+        firebaseRef.setValue(textField.text)
 
+        return true;
+    }
+    
+    func setupTextField(body: String) {
+        
+        textField = UITextField(frame: CGRectMake(0,0,300,100))
+        textField.text = body
+        textField.delegate = self
+        textField.borderStyle = UITextBorderStyle.RoundedRect
+        textField.layer.position = CGPoint(x:self.view.bounds.width/2,y:100);
+        self.view.addSubview(textField)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
