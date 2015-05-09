ec668991aaaf28809c8172cfffde15cde0556e87
shoya <12c1055@gmail.com>
2015-05-09 14:47:15 +0900

    first commit

---
+++ RealmFire/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  RealmFire
+//
+//  Created by shoya on 5/9/15.
+//  Copyright (c) 2015 com.moru. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+
+class Book: Object {
+    dynamic var title = ""
+    dynamic var author = ""
+}
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
